9dfa29bd172a8c74476266016030e636ad32bbc3
vivek1504 <jadhavvivek2743@gmail.com>
2024-09-15 12:41:21 +0530

    added-2

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,24 @@ import ConnectWallet from './Pages/ConnectWallet.jsx';
 import Auth0ProviderWithHistory from './auth0Provider.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignupForm from './Pages/SignUp.jsx';
-<<<<<<< HEAD
-import ProjectCard from './Components/ProjectCard.jsx';
-import TokenPage from './Pages/TokenPage.jsx';
-import Portfolio from './Pages/Portfolio.jsx';
 import MintToken from './Pages/MintToken.jsx';
-import Herosection from './Pages/heroSection.jsx';
-import MintTokenComponent from './Components/MintTokenComponent.jsx';
-=======
 import TransactionPage from './Pages/TransactionPage.jsx';
 import HeroSection from './Pages/HeroSection.jsx';
->>>>>>> b828a32fdb38f79b5f4c0eae7205b99741b42afe
 
 
 function App() {
   return (
     <>
-
-
-
       {/* <TokenPage /> */}
       {/* <MintTokenComponent /> */}
-      <MintToken />
+    
 
       {/* <Portfolio /> */}
       {/* <MintToken /> */}
 
       {/* <Herosection /> */}
      
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Auth0ProviderWithHistory>
           <RecoilRoot>
             <Routes>
@@ -40,10 +29,11 @@ function App() {
               <Route path="/signup" element={<SignupForm />} />
               <Route path='/transaction' element={<TransactionPage></TransactionPage>}></Route>
               <Route path='/hero' element={<HeroSection></HeroSection>}></Route>
+              <Route path='/mintToken' element={<MintToken></MintToken>}></Route>
             </Routes>
           </RecoilRoot>
         </Auth0ProviderWithHistory>
-      </BrowserRouter> */}
+      </BrowserRouter> 
 
     </>
   );
